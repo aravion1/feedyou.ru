6898d51c51c28308c8183572ac95800d20248286
aravion1 <aravion1@gmail.com>
2025-08-13 01:28:07 +0700

    Добавил метрику

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,6 +13,19 @@
             crossorigin="anonymous"></script>
 </head>
 <body class="bg-dark">
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103720534', 'ym');
+
+    ym(103720534, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/103720534" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 <header>
     <div class="container mb-5">
         <nav class="navbar navbar-expand-lg navbar-dark p-2">
